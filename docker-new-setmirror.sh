bf599c920ddaeb992a6db44405a33fb95ab6a9c9
ethanzhu <ethanzhu@qq.com>
2016-08-05 13:55:34 +0800

    添加时速云镜像加速器

--- docker-new-setmirror.sh
+++ docker-new-setmirror.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 # by ehtnahzu@qq.com
-# 最新版设置docker镜像源脚本 使用源：https://docker.mirrors.ustc.edu.cn
+# 最新版设置docker镜像源脚本 可以手动添加镜像源到MIRROR_URL
 # echo a |sudo tee 1.txt
-MIRROR_URL='"https://docker.mirrors.ustc.edu.cn","http://3a35aff6.m.daocloud.io"'
+MIRROR_URL='"https://docker.mirrors.ustc.edu.cn","http://3a35aff6.m.daocloud.io","http://ethanzhu.m.tenxcloud.net"'
 echo '{
     "registry-mirrors": ['$MIRROR_URL']
 }' | sudo tee /etc/docker/daemon.json
